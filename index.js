136a789df215b282808fccac8c740047191441d5
zimzim <zimzim@youcanstar.com>
2020-10-06 10:56:28 +0900

    security

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+const {User} = require('./models/user');
+const config = require('./config/key');
 
-mongoose.connect('mongodb+srv://Sandy:1234@boilerplate.4z9dn.mongodb.net/<dbname>?retryWrites=true&w=majority', 
+//db connection
+mongoose.connect(config.mongoURI, 
     {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: true})
     .then(()=> console.log('Mongodb Connected...'))
     .catch(err=> console.log(err));
+// x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+// json
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Ello Orld!'));
+app.post('/register', (req, res) => {
+    const user = new User(req.body);
+    user.save((err, userInfo)=>{
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({success: true})
+    });
+})
 
 app.listen(port, () => console.log(`boiler-plate listening on port ${port}`));
 
